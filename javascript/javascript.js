2292cac448bc04d5ac1f2c5f4da7427644efa470
Anouar <rachel.vanpuywelde@gmail.com>
2023-02-21 15:16:42 +0100

    javacript

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,3 +1,9 @@
 document.write("Mutation");
 let nom = prompt("quel est votre nom");
-alert(nom);
+let prenom = prompt("quel est votre prenom");
+alert("Je m'appelle "+ prenom +" "+ nom);
+
+let nombre1 = prompt("quel est le premier nombre");
+let nombre2 = prompt("quel est le deuxieme nombre");
+let resultat = nombre1*nombre2;
+alert("le resultat est :" + resultat);
