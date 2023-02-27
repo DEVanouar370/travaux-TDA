dd93eb4241114efea5e8abb4932afa52d9f48430
Anouar <rachel.vanpuywelde@gmail.com>
2023-02-27 10:08:54 +0100

    javascript

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,13 +1,4 @@
-document.write("Mutation");
-let nom = prompt("quel est votre nom");
-let prenom = prompt("quel est votre prenom");
-alert("Je m'appelle "+ prenom +" "+ nom);
 
-let nombre1 = prompt("quel est le premier nombre");
-let nombre2 = prompt("quel est le deuxieme nombre");
-let resultat = nombre1*nombre2;
-alert("le resultat est :" + resultat);
 
-let degrescelsius = prompt("quel est la temperature");
-let calcule = (degrescelsius*9/5)+32; 
-alert("resultat " + calcule);
+
+  
